656597f9f07bc912f0bd40a017a5a4feb6a3b384
metool <metool@gmail.com>
2018-05-22 10:28:18 -0700

    Fixing code

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def parse_requirements(filename):
     return [line for line in lineiter if line and not line.startswith("#")]
 
 
-install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
+install_reqs = parse_requirements('requirements.txt')
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
